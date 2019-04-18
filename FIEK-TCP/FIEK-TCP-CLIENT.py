fb4467118ff03444f5f5f40c59da1332a71fcef3
Alba <alba_lokaj@hotmail.com>
2019-04-18 21:44:00 +0200

    Permiresim ne kod

--- FIEK-TCP/FIEK-TCP-CLIENT.py
+++ FIEK-TCP/FIEK-TCP-CLIENT.py
@@ -1,22 +1,33 @@
 import socket
 
-serverName = "localhost"
-port = 9999
+serverName = input("Shkruaj hostin: ")
+port = int(input("Shkruaj portin: "))
+
+
 soketi = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 soketi.connect((serverName, port))
 
-while 1:
-    answer = str(soketi.recv(1024), "utf-8")
-    if answer == "quit":
-        print("Socketi u mbyll...")
-        soketi.close()
-        break
-    print("Server:  " + answer)
-    data = input('\nYou:  ')
-    if data == "quit":
-        soketi.sendall(str.encode(data))
-        print("Socketi u mbyll...")
-        soketi.close()
-        break
-    if len(str.encode(data)) > 0:
-        soketi.sendall(str.encode(data))
+while True:
+    print("Shkruaj njeren nga kerkesat: ")
+    kerkesa = input("Kerkesa: ")
+    format1 = str(kerkesa.strip())
+    format = str(format1.upper())
+    if format == "BISEDA":
+        soketi.sendall(str.encode(format))
+        while 1:
+            answer = str(soketi.recv(1024), "utf-8")
+            if answer == "quit":
+                print("Biseda perfundoi...")
+                break
+            print("Server:  " + answer)
+            data = input('You:  ')
+            if data == "quit":
+                soketi.sendall(str.encode(data))
+                print("Biseda perfundoi...")
+                break
+            if len(str.encode(data)) > 0:
+                soketi.sendall(str.encode(data))
+    else:
+            soketi.sendall(str.encode(format))
+            answer = str(soketi.recv(1024), "utf-8")
+            print(answer)
--- FIEK-TCP/FIEK-TCP-SERVER.py
+++ FIEK-TCP/FIEK-TCP-SERVER.py
@@ -2,6 +2,7 @@ import socket
 import datetime
 import random
 import math
+from _thread import *
 
 host = "localhost"
 port = 9999
@@ -24,61 +25,53 @@ try:
 except socket.error as error:
     print("Error gjate lidhjes se hostit me portin: " + str(error))
 
-#Lidhja e serverit me klientin
-while 1:
-    connection, address = soketi.accept()
-    print("Serveri u lidh me klientin me IP adrese " + str(address[0]) + " ne portin " + str(address[1]))
-
 
 
 #...........................Metoda IPADRESA......................................
 def IPADRESA():
-    print("IP adresa e klientit eshte: " + str(address[0]))
+    return "IP adresa e klientit eshte: " + str(address[0])
 
 
 #...........................Metoda NUMRIIPORTIT...................................
 def NUMRIIPORTIT():
-    print("Numri i portit te klientit eshte: " + str(address[1]))
+    return "Klienti është duke përdorur portin: " + str(address[1])
 
 
 #...........................Metoda BASHKETINGELLORE...............................
 def BASHKETINGELLORE(teksti):
-    teksti = input("Shkruani nje tekst: ")
-    teksti.strip()
-    zanoret = {'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'}
+    zanoret = "aeiouyAEIOUY"
     num = 0
-    for i in range (len(list(teksti))):
-        if list(teksti)[i].isaplha == True & list(teksti)[i] not in zanoret:
+    for i in teksti:
+        if i.isalpha() & (not i in zanoret):
             num += 1
-    print("Numri i bashketingelloreve ne tekstin " + teksti + " eshte " + str(num))
+    return "Teksti i pranuar permban " + str(num) + " bashketingellore"
 
 
 #...........................Metoda PRINTIMI........................................
 def PRINTIMI(fjalia):
-    fjalia = input("Shkruani nje fjali: ")
     return fjalia.strip()
 
 
 #...........................Metoda EMRIIKOMPJUTERIT................................
 def EMRIIKOMPJUTERIT():
     try:
-        print("Emri i hostit eshte "+ str(socket.gethostbyaddr(host)))
+        return "Emri i klientit është "+ str(socket.gethostbyaddr(host)[0])
     except socket.error as error:
-        print("Emri i hostit nuk mund te gjendet")
+        return "Emri i klientit nuk dihet."
 
 
 #...........................Metoda KOHA............................................
 def KOHA():
     x = datetime.datetime.now()
-    print("Koha aktuale ne server eshte " + x.strftime("%H:%M:%S"))
+    return x.strftime("%d.%m.%Y %H:%M:%S %p")
 
 
 #...........................Metoda LOJA............................................
 def LOJA():
-    print("(", end="")
-    for x in range(6):
-        print(random.randint(1, 49), end=",")
-    print(random.randint(1, 49), end=")")
+    vargu = []
+    for x in range(7):
+       vargu.append(random.randint(1, 49))
+    return str(vargu)
 
 
 #...........................Metoda FIBONACCI........................................
@@ -86,56 +79,95 @@ def FIBONACCI(numri):
     num1 = 0
     num2 = 1
     i = 0
-    if numri < 0:
-        print("Numri i plote pas funksionit duhet te jete pozitiv")
-    elif numri == 0:
-        print(num1)
-    elif numri == 1:
-        print(num2)
-    else:
-        while i <= numri:
-            shuma = num1 + num2
-            num1 = num2
-            num2 = shuma
-            i += 1
-    return num2
+    if int(numri) < 0:
+        return "Numri i plote pas funksionit duhet te jete pozitiv"
+    elif int(numri) == 0:
+        return num1
+    elif int(numri) == 1:
+        return num2
+    while i < int(numri):
+        shuma = num1 + num2
+        num1 = num2
+        num2 = shuma
+        i += 1
+    return str(num1)
 
 
 # ...........................Metoda KONVERTIMI......................................
 def KONVERTIMI(opsioni, numri):
-        numri = float(numri)
-        if opsioni == "KilowattToHorsepower":
-            return numri * 1.341
-        elif opsioni == "HorsepowerToKilowatt":
-            return numri / 1.341
-        elif opsioni == "DegreesToRadians":
-            return numri * math.pi / 180
-        elif opsioni == "RadiansToDegrees":
-            return numri * 180 / math.pi
-        elif opsioni == "GallonsToLiters":
-            return numri * 3.785
-        elif opsioni == "LitersToGallons":
-            return numri / 3.785
-        else:
-            print("Opsioni nuk ekziston!")
+    numri = float(numri)
+    if opsioni == "KILOWATTTOHORSEPOWER":
+        return str(numri * 1.341)
+    elif opsioni == "HORSEPOWERTOKILOWATT":
+        return str(numri / 1.341)
+    elif opsioni == "DEGREESTORADIANS":
+        return str(numri * math.pi / 180)
+    elif opsioni == "RADIANSTODEGREES":
+        return str(numri * 180 / math.pi)
+    elif opsioni == "GALLONSTOLITERS":
+        return str(numri * 3.785)
+    elif opsioni == "LITERSTOGALLONS":
+        return str(numri / 3.785)
 
 
 #...........................Metoda BISEDA...........................................
-def BISEDA(connection):
+def BISEDA():
     while 1:
-        data = input("\nYou: ")
+        data = input("You: ")
         if data == 'quit':
-            print("Lidhja me klientit u mbyll...")
             connection.send(str.encode(data))
-            connection.close()
+            print("Biseda perfundoi...")
             break
         if len(str.encode(data)) > 0:
                 connection.send(str.encode(data))
                 rec = str(connection.recv(1024), "utf-8")
                 if rec == "quit":
-                    print("Klienti ka mbyllur lidhjen...")
-                    connection.close()
+                    print("Biseda perfundoi...")
                     break
                 print("Client:  " + rec)
+    return
 
+#...........................Metoda Thread...........................................
+def thread(connection, address):
+    while True:
+        kerkesa = str(connection.recv(1024), "utf-8")
+        kerkesa = kerkesa.split(" ")
+        if kerkesa[0] == "QUIT":
+            print("Klienti ka mbyllur lidhjen...")
+            break
+        if kerkesa[0] == "IPADRESA":
+            connection.send(str.encode(IPADRESA()))
+        elif kerkesa[0] == "NUMRIIPORTIT":
+            connection.send(str.encode(NUMRIIPORTIT()))
+        elif kerkesa[0] == "BASHKETINGELLORE":
+            teksti = ""
+            teksti = str.join(" ", kerkesa[1:])
+            connection.send(str.encode(BASHKETINGELLORE(teksti)))
+        elif kerkesa[0] == "PRINTIMI":
+            fjalia = ""
+            fjalia = str.join(" ", kerkesa[1:])
+            connection.send(str.encode(PRINTIMI(fjalia)))
+        elif kerkesa[0] == "EMRIIKOMPJUTERIT":
+            connection.send(str.encode(EMRIIKOMPJUTERIT()))
+        elif kerkesa[0] == "KOHA":
+            connection.send(str.encode(KOHA()))
+        elif kerkesa[0] == "LOJA":
+            connection.send(str.encode(LOJA()))
+        elif kerkesa[0] == "FIBONACCI":
+            numri = kerkesa[1]
+            connection.send(str.encode(FIBONACCI(numri)))
+        elif kerkesa[0] == "KONVERTIMI":
+            opsioni = kerkesa[1]
+            numri = kerkesa[2]
+            connection.send(str.encode(KONVERTIMI(opsioni, numri)))
+        elif kerkesa[0] == "BISEDA":
+            BISEDA()
+        else:
+            connection.send(str.encode("Kerkesa nuk ekziston!"))
+    return
 
+#Lidhja e serverit me klientin
+while 1:
+    connection, address = soketi.accept()
+    print("Serveri u lidh me klientin me IP adrese " + str(address[0]) + " ne portin " + str(address[1]))
+    start_new_thread(thread, (connection, address))
